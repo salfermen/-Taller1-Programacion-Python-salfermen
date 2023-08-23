ef4fb04f51825636cbb7b6c52251b56c4f05aa24
Salim Ferez Mendez <123603908+salfermen@users.noreply.github.com>
2023-08-22 19:50:07 -0500

    Create Division.py

---
+++ Division.py
@@ -0,0 +1,10 @@
+print("Bienvenido, por favor ingrese el primer número ")
+N1 = int(input())
+
+print("Ingrese el segundo número ")
+N2 = int(input())
+
+if N1 % N2 == 0:
+    print("La division es exacta")
+else:
+    print("La division no es exacta")
